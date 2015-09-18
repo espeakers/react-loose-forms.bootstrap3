63b9af5d74a635f93b14b48451f00e4b7aaa0c7b
Matthew Wright <mwright@espeakers.com>
2015-09-17 18:20:49 -0600

    embeded icon for HelpTooltip

--- layouts/HelpTooltip.js
+++ layouts/HelpTooltip.js
@@ -1,6 +1,8 @@
 var dd = require("react-dd");
 var HoverMixin = require("react-hover-mixin");
 
+require("./HelpTooltip.css.js");
+
 module.exports = dd.createClass({
   mixins: [HoverMixin],
   hoverable_not_relative_to_parent_bounding_rect: true,
@@ -20,17 +22,7 @@ module.exports = dd.createClass({
         onMouseLeave: this.hoverable_onMouseLeave,
         onClick: this.__onClick
       },
-      dd.span({style: {
-        display: "inline-block",
-        width: "1.2em",
-        height: "1.2em",
-        lineHeight: "1.2em",
-        borderRadius: "1em",
-        textAlign: "center",
-        fontWeight: "bold",
-        color: "#FFFFFF",
-        background: this.state.hover ? "#23527c" : "#337ab7"
-      }}, "?"),
+      dd.i({className: "react-bootstrap3-tooltip-icon react-bootstrap3-tooltip-icon-help-circled"}),
       dd.div({className: 'popover top', ref: 'hoverable', style: {
           display: this.state.hover ? 'block' : 'none',//hide show so that we can keep access to the dom node's width height in componentDidUpdate
           width: content ? Math.max(70, content.length * 14) + 'px' : 'auto',//aprox the width for the content, note that .popover class sets a max-width, so no worries
